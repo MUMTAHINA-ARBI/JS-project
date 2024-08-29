0312343ece482b607d96f813ae2773e7317c4c4a
MUMTAHINA-ARBI <mum135rb@gmail.com>
2024-08-29 22:37:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+alert("Mumtahina");
+alert(25);
+document.write("Arbi");
+console.log("hello");
+var num= 20;
+num= toString(num);//sting convert
+num= parseInt(num); //integer convert
+num= parseFloat(num); //float convert
+var number=2.5674
+var text="nnnm"
+var t=prompt("enter your name:"); // user input & return string
+var h=parseInt(prompt("enter your number:")); // user input & return integer
+h=parseInt(h,10); 
+document.write(text.charAt(2)+"<br/>"); //number 2 index character
+document.write(text.toUpperCase()+"<br/>");
+document.write(text.concat(t)+"<br/>");
+document.write(text.slice(0,1)+"<br/>"); 
+console.log(typeof(num));
+console.log(number.toFixed(2)); //float number & string return
+console.log(number.toPrecision(2)); //total number & string return
+console.log(typeof(Number(false)+"<br/>")); // datatype number convert
+document.write("arbi" + "mumtahina" +"<br/>") //string concatination
+document.write(text.length +"<br/>" ); //number of length of a string
+
+document.write(t.length  );
+/*  Arithmetic operator +,-,* ,/,% (modulus),**(exponent), ++ (increment),--(decrement),
+++a(pre-increment),a++(post-increment) */
+//assignment operator =,+=,-=,*=,/=,%=,**=
+var x = 4;
+x += 6;  //x= x+6
+console.log(x);
+
